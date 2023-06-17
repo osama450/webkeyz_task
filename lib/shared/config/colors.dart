5be8e2af83ab9465982e6c1042bf7d6fe7124d6e
osama450 <osama.kaboo55@gmail.com>
2023-06-18 00:36:22 +0100

    Fix app colors

--- lib/shared/config/colors.dart
+++ lib/shared/config/colors.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 abstract class AppColors {
   static const Color primaryColor = Color.fromRGBO(54, 56, 64, 1);
   static const Color secondaryPrimary = Color(0xFFEDA841);
-  static const Color backgroundColor = Color(0xff2E3139);
+  static const Color backgroundColor = Color.fromRGBO(40, 41, 50, 1);
 }
