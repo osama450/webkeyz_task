a119910e7566bab63f1d4eac266d2b9c1ae841e6
osama450 <osama.kaboo55@gmail.com>
2023-06-18 01:55:55 +0100

    Add unit test

--- test/modules/home/cubit/cubit_test.dart
+++ test/modules/home/cubit/cubit_test.dart
@@ -1,17 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:webkeyz_task/modules/home/cubit/cubit.dart';
-import 'package:webkeyz_task/shared/network/dio/dio_helper.dart';
 import 'package:webkeyz_task/shared/network/repository.dart';
 import 'package:webkeyz_task/shared/network/repository_impl.dart';
 
 void main() {
   late Repository repository;
-  DioHelper dioHelper;
 
   setUp(() {
     repository = RepoImpl(dioHelper: null);
   });
-  group('Cart', () {
+  group('HomeCubit', () {
     test('Adding an item to the cart should increase the total count',
         () async {
       // Arrange
@@ -24,6 +22,19 @@ void main() {
 
       // Assert
       expect(cart, 1);
+    });
+
+    test('Check if data get from local successfully', () async {
+      // Arrange
+
+      final HomeCubit cubit = HomeCubit(repository);
+
+      // Act
+      await cubit.addToCart();
+      int productLength = cubit.productList.length;
+
+      // Assert
+      expect((productLength != 0), false);
     });
   });
 }
